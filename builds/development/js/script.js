f8940fbe8314f53fa6431c81f64d474704d58e5c
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-02-03 14:18:22 -0800

    styling portrait article preview

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -21,11 +21,26 @@ $(function(){
 
     //$('.featured-image').imagefill();
 
-    $('.featured-image').each(function(i, counter){
+    $('.featured-wrapper .featured-image').each(function(i, counter){
         var image = $(this).data('featured');
         $(this).css('background', "url('"+image+"') center center");
         $(this).css('background-size', 'cover');
     })
+
+    $('.rating-wrapper').each(function(i, counter){
+        var rating = $(this).data('rating');
+
+        $(this).html("");
+
+        for(var index = 0; index < 5; index++){
+            if(index < rating){
+                $(this).append("<i class='fa fa-circle rated'></i>")
+            }
+            else{
+                $(this).append("<i class='fa fa-circle'></i>")
+            }
+        }
+    });
 });
 
 $(function () {

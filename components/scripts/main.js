bd377a3c0d8e9dde4f4f6ecd6c24acd3f5074233
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-02-03 09:24:42 -0800

    styling main featured image

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -4,4 +4,17 @@ $(function(){
         cssArrows: false,
         delay: 200
     });
+
+    $('.featured-image').find('img').each(function(){
+        var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+        $(this).addClass(imgClass);
+    })
+
+    //$('.featured-image').imagefill();
+
+    $('.featured-image').each(function(i, counter){
+        var image = $(this).data('featured');
+        $(this).css('background', "url('"+image+"') center center");
+        $(this).css('background-size', 'cover');
+    })
 });

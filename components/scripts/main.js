7634cdb3516a6f794035eab022462d6cf161adf9
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-02-04 02:15:30 -0800

    reformat mobile and add parallax effect on it and footer

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,35 +1,35 @@
-$(function(){
+$(function () {
     var navigation = $('#navigation').superfish({
         speed: 'fast',
         cssArrows: false,
         delay: 200
     });
 
-    $('.featured-image').find('img').each(function(){
-        var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+    $('.featured-image').find('img').each(function () {
+        var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
         $(this).addClass(imgClass);
     })
 
-    //$('.featured-image').imagefill();
-
-    $('.featured-wrapper .featured-image').each(function(i, counter){
+    $('.featured-wrapper .featured-image').each(function (i, counter) {
         var image = $(this).data('featured');
-        $(this).css('background', "url('"+image+"') center center");
+        $(this).css('background', "url('" + image + "') center center");
         $(this).css('background-size', 'cover');
     })
 
-    $('.rating-wrapper').each(function(i, counter){
+    $('.rating-wrapper').each(function (i, counter) {
         var rating = $(this).data('rating');
 
         $(this).html("");
 
-        for(var index = 0; index < 5; index++){
-            if(index < rating){
+        for (var index = 0; index < 5; index++) {
+            if (index < rating) {
                 $(this).append("<i class='fa fa-circle rated'></i>")
             }
-            else{
+            else {
                 $(this).append("<i class='fa fa-circle'></i>")
             }
         }
     });
+
+    $(window).stellar();
 });

6498f43c5cc975c806719df4b172cb2bc3a33815
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-02-10 21:13:52 -0800

    styling admin sidebar and add toggle button for mobile

--- builds/development/js/admin.js
+++ builds/development/js/admin.js
@@ -1,6 +1,36 @@
 $(function () {
-    console.log($(window).height());
-    if($(window).height() < 450){
-        $(".login-footer").hide();
-    }
+
+    // Toggle script ------------------------------------------------------------------------
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    // Resize event to check browser viewport ----------------------------------------------
+    var wrapper = $("#wrapper");
+    $(window).resize(function () {
+        if($(window).height() < 450){
+            $(".login-footer").hide();
+        }
+
+        if($(window).height() < 500){
+            $(".copyright").css('position', 'relative');
+        }
+
+        if($(window).width() < 767){
+            if(!wrapper.hasClass('toggled')){
+                wrapper.addClass('toggled');
+            }
+        }
+        else{
+            if(wrapper.hasClass('toggled')){
+                wrapper.removeClass('toggled');
+            }
+        }
+    });
+
+    $("#sidebar-wrapper").niceScroll({
+        cursorcolor: '#4dc4d2',
+        cursorborder: 'none'
+    });
 });

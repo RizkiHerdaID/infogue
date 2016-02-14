8f42b6d14e02e7f75f3b248fcec03d249c1a44d0
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-02-14 08:47:38 -0800

    add admin reset page and separate resize function into file

--- builds/development/js/admin.js
+++ builds/development/js/admin.js
@@ -6,29 +6,6 @@ $(function () {
         $("#wrapper").toggleClass("toggled");
     });
 
-    // Resize event to check browser viewport ----------------------------------------------
-    var wrapper = $("#wrapper");
-    $(window).resize(function () {
-        if($(window).height() < 450){
-            $(".login-footer").hide();
-        }
-
-        if($(window).height() < 500){
-            $(".copyright").css('position', 'relative');
-        }
-
-        if($(window).width() < 767){
-            if(!wrapper.hasClass('toggled')){
-                wrapper.addClass('toggled');
-            }
-        }
-        else{
-            if(wrapper.hasClass('toggled')){
-                wrapper.removeClass('toggled');
-            }
-        }
-    });
-
     $("html").niceScroll({
         cursorcolor: '#4dc4d2',
         cursorborder: 'none'
@@ -175,3 +152,35 @@ $(function () {
         });
     }
 });
+/**
+ * Created by Workstation on 2/14/2016.
+ */
+
+// Resize event to check browser viewport
+var wrapper = $("#wrapper");
+
+setLayout(0);
+
+$(window).resize(function () {
+    setLayout(300);
+});
+
+function setLayout(duration){
+    if($(window).height() < 450){
+        $(".login-footer").fadeOut(duration);
+    }
+    else{
+        $(".login-footer").fadeIn(duration);
+    }
+
+    if($(window).width() < 767){
+        if(!wrapper.hasClass('toggled')){
+            wrapper.addClass('toggled');
+        }
+    }
+    else{
+        if(wrapper.hasClass('toggled')){
+            wrapper.removeClass('toggled');
+        }
+    }
+}

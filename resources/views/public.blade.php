17ab3310d87d1d888f4e1cf967ae0813fcdf467a
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-03-04 17:03:29 +0700

    add meta url for javascript reference

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="{{ $site_settings['Owner'] }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="url" content="{{ Request::url() }}" />
 
     <meta property="og:url" content="{{ Request::url() }}" />
     <meta property="og:type" content="website" />

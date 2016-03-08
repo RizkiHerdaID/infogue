5e04eec9cd3be256cf7a3c62e51eb7e10e5b6ddc
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-03-08 14:53:05 +0700

    add margin on common errors alert

--- resources/views/errors/common.blade.php
+++ resources/views/errors/common.blade.php
@@ -1,10 +1,13 @@
 @if (count($errors) > 0)
 <div class="alert alert-danger" style="text-align: left;">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="font-size: 16px">
+        <span aria-hidden="true">&times;</span>
+    </button>
     <strong>Whoops! Something went wrong!</strong>
 
     <br>
 
-    <ul style="margin-bottom: 0; -webkit-padding-start: 20px;">
+    <ul style="margin-bottom: 0; -webkit-padding-start: 20px; margin-left: 20px">
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach

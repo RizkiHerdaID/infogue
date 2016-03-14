3aaf5e8d270e916b42cbeeb122cf78b3e9001d9a
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-03-14 20:33:24 +0700

    commenting article model

--- app/Article.php
+++ app/Article.php
@@ -5,43 +5,83 @@ namespace Infogue;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Article extends Model
 {
     use SoftDeletes;
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
     protected $dates = ['deleted_at'];
 
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = ['deleted_at'];
 
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
 
-        static::addGlobalScope('latest', function(Builder $builder) {
+        static::addGlobalScope('latest', function (Builder $builder) {
             $builder->orderBy('articles.created_at', 'desc');
         });
     }
 
+    /**
+     * additional query scope, select published article only.
+     *
+     * @param $query
+     * @return mixed
+     */
     public function scopePublished($query)
     {
         return $query->where('articles.status', 'published');
     }
 
+    /**
+     * Many-to-one relationship, article author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function contributor()
     {
         return $this->belongsTo('Infogue\Contributor');
     }
 
+    /**
+     * Many-to-one relationship, article subcategory.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function subcategory()
     {
         return $this->belongsTo('Infogue\Subcategory');
     }
 
+    /**
+     * Retrieve accumulation of article rating.
+     *
+     * @return mixed
+     */
     public function rating()
     {
         return $this->hasOne('Infogue\Rating')
@@ -49,18 +89,43 @@ class Article extends Model
             ->groupBy('article_id');
     }
 
+    /**
+     * One-to-many relationship article rating.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function ratings()
     {
         return $this->hasMany('Infogue\Rating');
     }
 
+    /**
+     * Many-to-many relationship, article tags.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function tags()
     {
         return $this->belongsToMany('Infogue\Tag', 'article_tags')->withTimestamps();
     }
 
+    /**
+     * Find out if an article related to another.
+     *
+     * @param $id
+     * @return mixed
+     */
     public function related($id)
     {
+        /*
+         * --------------------------------------------------------------------------
+         * Populate related article by tags
+         * --------------------------------------------------------------------------
+         * Retrieve article tags by article id, the idea of related article is
+         * comparing article tags with another so this part is try to collect array
+         * of article tags id and take top 5 with similar tags.
+         */
+
         $tags = $this->published()
             ->join('article_tags', 'articles.id', '=', 'article_id')
             ->join('tags', 'tags.id', '=', 'tag_id')
@@ -80,6 +145,12 @@ class Article extends Model
         return $related;
     }
 
+    /**
+     * Find out most popular article in last 3 months with most viewed.
+     *
+     * @param int $take
+     * @return mixed
+     */
     public function mostPopular($take = 10)
     {
         $popular = $this->preArticleQuery()->published()
@@ -91,6 +162,11 @@ class Article extends Model
         return $this->preArticleModifier($popular);
     }
 
+    /**
+     * Find out most ranked article in passed a year.
+     *
+     * @return mixed
+     */
     public function mostRanked()
     {
         $ranking = $this->published()
@@ -105,12 +181,17 @@ class Article extends Model
         return $ranking;
     }
 
+    /**
+     * Select article where state is headline.
+     *
+     * @param bool|true $is_featured
+     * @return mixed
+     */
     public function headline($is_featured = true)
     {
-        if($is_featured){
+        if ($is_featured) {
             $headline = $this->published()->where('state', 'headline')->take(4)->get();
-        }
-        else{
+        } else {
             $articles = $this->preArticleQuery()->published()->where('state', 'headline')->paginate(9);
 
             $headline = $this->preArticleModifier($articles);
@@ -119,12 +200,17 @@ class Article extends Model
         return $headline;
     }
 
+    /**
+     * Select article where state is trending.
+     *
+     * @param bool|true $is_featured
+     * @return mixed
+     */
     public function trending($is_featured = true)
     {
-        if($is_featured){
+        if ($is_featured) {
             $trending = $this->published()->where('state', 'trending')->take(4)->get();
-        }
-        else{
+        } else {
             $articles = $this->preArticleQuery()->published()->where('state', 'trending')->paginate(9);
 
             $trending = $this->preArticleModifier($articles);
@@ -133,9 +219,15 @@ class Article extends Model
         return $trending;
     }
 
+    /**
+     * Retrieve published latest article.
+     *
+     * @param bool|true $is_featured
+     * @return mixed
+     */
     public function latest($is_featured = true)
     {
-        if($is_featured){
+        if ($is_featured) {
             $trending = $this->select('id')
                 ->published()
                 ->where('state', 'trending')
@@ -143,8 +235,7 @@ class Article extends Model
                 ->pluck('id')->toArray();
 
             $latest = $this->published()->whereNotIn('id', $trending)->take(4)->get();
-        }
-        else{
+        } else {
             $articles = $this->preArticleQuery()->published()->paginate(9);
 
             $latest = $this->preArticleModifier($articles);
@@ -153,6 +244,11 @@ class Article extends Model
         return $latest;
     }
 
+    /**
+     * Retrieve random article, it can be infinity.
+     *
+     * @return mixed
+     */
     public function random()
     {
         $articles = $this->preArticleQuery()->orderByRaw("RAND()")->published()->paginate(9);
@@ -162,40 +258,67 @@ class Article extends Model
         return $random;
     }
 
+    /**
+     * Retrieve all published article as archive or by contributor.
+     *
+     * @param $data
+     * @param $by
+     * @param $sort
+     * @param null $contributor
+     * @return mixed
+     */
     public function archive($data, $by, $sort, $contributor = null)
     {
         $archive = $this->preArticleQuery();
 
-        if($contributor == null){
+        /*
+         * --------------------------------------------------------------------------
+         * Type archive
+         * --------------------------------------------------------------------------
+         * Select archive by contributor or select all article archive, contributor
+         * archive use in show article on contributor profile.
+         */
+
+        if ($contributor == null) {
             $archive->published();
-        }
-        else{
+        } else {
             $archive->where('contributor_id', $contributor);
         }
 
-        if($data == 'trending'){
+        /*
+         * --------------------------------------------------------------------------
+         * Data filtering
+         * --------------------------------------------------------------------------
+         * Filter data by state, category, article, otherwise do not add criteria
+         * or condition.
+         */
+
+        if ($data == 'trending') {
             $archive->where('state', 'trending');
-        }
-        else if($data == 'popular'){
+        } else if ($data == 'popular') {
             $archive->where('articles.created_at', '>', Carbon::now()->addMonth(-3))->orderBy('view', 'desc');
-        }
-        else if($data == 'headline'){
+        } else if ($data == 'headline') {
             $archive->where('state', 'headline');
-        }
-        else if($data != 'all-data'){
+        } else if ($data != 'all-data') {
             $archive->where('category', 'like', "%{$data}%");
         }
 
-        if($sort == 'random'){
+        /*
+         * --------------------------------------------------------------------------
+         * Sorting the data
+         * --------------------------------------------------------------------------
+         * Just simply data order, sort by date, title, star, view in ascending,
+         * descending or shuffle list.
+         */
+
+        if ($sort == 'random') {
             $archive->orderByRaw("RAND()");
-        }
-        else{
+        } else {
             $sort_by = $by;
 
-            if($by == 'date'){
+            if ($by == 'date') {
                 $sort_by = 'created_at';
-            }
-            else if($by == 'star'){
+            } else if ($by == 'star') {
                 $sort_by = 'total_rating';
             }
 
@@ -205,11 +328,30 @@ class Article extends Model
         return $this->preArticleModifier($archive->paginate(12));
     }
 
+    /**
+     * Retrieve all article without published scope use in admin page.
+     *
+     * @param $data
+     * @param $status
+     * @param $by
+     * @param $sort
+     * @param null $query
+     * @return mixed
+     */
     public function retrieveArticle($data, $status, $by, $sort, $query = null)
     {
         $articles = $this->preArticleQuery();
 
-        if($query != null && $query != ''){
+        /*
+         * --------------------------------------------------------------------------
+         * Searching
+         * --------------------------------------------------------------------------
+         * Check if query passed as param and is not empty, try guess similarity by
+         * title, name of author, category name, subcategory name, or even the
+         * content itself.
+         */
+
+        if ($query != null && $query != '') {
             $articles
                 ->where('title', 'like', "%{$query}%")
                 ->orWhere('name', 'like', "%{$query}%")
@@ -218,42 +360,65 @@ class Article extends Model
                 ->orWhere('content', 'like', "%{$query}%");
         }
 
-        if($status != 'all'){
+        /*
+         * --------------------------------------------------------------------------
+         * Status filter
+         * --------------------------------------------------------------------------
+         * Except all status filter find article by certain status.
+         */
+
+        if ($status != 'all') {
             $articles->where('articles.status', $status);
         }
 
-        if($data == 'trending'){
+        /*
+         * --------------------------------------------------------------------------
+         * Kind of data
+         * --------------------------------------------------------------------------
+         * Data filter like in archive filter, select data by state, category,
+         * article, otherwise do not add criteria or condition.
+         */
+
+        if ($data == 'trending') {
             $articles->where('state', 'trending');
-        }
-        else if($data == 'popular'){
+        } else if ($data == 'popular') {
             $articles->where('articles.created_at', '>', Carbon::now()->addMonth(-3))->orderBy('view', 'desc');
-        }
-        else if($data == 'headline'){
+        } else if ($data == 'headline') {
             $articles->where('state', 'headline');
-        }
-        else if($data != 'all'){
+        } else if ($data != 'all') {
             $articles->where('category', 'like', "%{$data}%");
         }
 
-        if($by == 'date'){
+        /*
+         * --------------------------------------------------------------------------
+         * Sorting the data
+         * --------------------------------------------------------------------------
+         * Just simply data order, sort by date, title, author, star, view in
+         * ascending, descending or shuffle list.
+         */
+
+        if ($by == 'date') {
             $articles->orderBy('created_at', $sort);
-        }
-        else if($by == 'title'){
+        } else if ($by == 'title') {
             $articles->orderBy('title', $sort);
-        }
-        else if($by == 'view'){
+        } else if ($by == 'view') {
             $articles->orderBy('view', $sort);
-        }
-        else if($by == 'author'){
+        } else if ($by == 'author') {
             $articles->orderBy('name', $sort);
-        }
-        else if($by == 'popularity'){
+        } else if ($by == 'popularity') {
             $articles->orderBy('total_rating', $sort);
         }
 
         return $this->preArticleModifier($articles->paginate(10));
     }
 
+    /**
+     * Search article by query, use in public page.
+     *
+     * @param $query
+     * @param int $take
+     * @return mixed
+     */
     public function search($query, $take = 10)
     {
         $result = $this->preArticleQuery()->published()
@@ -267,10 +432,15 @@ class Article extends Model
         return $this->preArticleModifier($result);
     }
 
+    /**
+     * Build basic of article select query.
+     *
+     * @return mixed
+     */
     public function preArticleQuery()
     {
         return $this->select(
-                DB::raw('
+            DB::raw('
                     articles.id,
                     slug,
                     title,
@@ -290,8 +460,8 @@ class Article extends Model
                     category_id,
                     category,
                     articles.created_at'
-                )
             )
+        )
             ->join('subcategories', 'subcategories.id', '=', 'subcategory_id')
             ->join('categories', 'categories.id', '=', 'category_id')
             ->join('contributors', 'contributors.id', '=', 'contributor_id')
@@ -299,14 +469,20 @@ class Article extends Model
             ->groupBy('articles.id');
     }
 
+    /**
+     * Article modifier needed for javascript template.
+     *
+     * @param $articles
+     * @return mixed
+     */
     public function preArticleModifier($articles)
     {
-        foreach($articles as $article):
+        foreach ($articles as $article):
 
-            $article->featured_ref = asset('images/featured/'.$article->featured);
+            $article->featured_ref = asset('images/featured/' . $article->featured);
             $article->article_ref = route('article.show', [$article->slug]);
             $article->contributor_ref = route('contributor.stream', [$article->username]);
-            $article->avatar_ref = asset('images/contributors/'.$article->avatar);
+            $article->avatar_ref = asset('images/contributors/' . $article->avatar);
             $article->category_ref = route('article.category', [str_slug($article->category)]);
             $article->subcategory_ref = route('article.subcategory', [str_slug($article->category), str_slug($article->username)]);
             $article->published_at = Carbon::parse($article->created_at)->format('d F Y');
@@ -317,18 +493,25 @@ class Article extends Model
         return $articles;
     }
 
-    public function navigateArticle($id, $direction, $total = 1){
-        if($direction == 'prev'){
+    /**
+     * Find out if there is next or previous article from current article id.
+     * 
+     * @param $id
+     * @param $direction
+     * @param int $total
+     * @return mixed
+     */
+    public function navigateArticle($id, $direction, $total = 1)
+    {
+        if ($direction == 'prev') {
             $article = $this->published()->where('id', '<', $id)->orderBy('id', 'desc')->take($total);
-        }
-        else{
+        } else {
             $article = $this->published()->where('id', '>', $id)->orderBy('id', 'asc')->take($total);
         }
 
-        if($total > 1){
+        if ($total > 1) {
             $article = $article->get();
-        }
-        else{
+        } else {
             $article = $article->first();
         }
 
